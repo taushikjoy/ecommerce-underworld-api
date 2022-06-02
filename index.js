235bda8c3a77f93a59f6720becd13e1e739443ab
Taushik Ahmed Joy <taushikjoy@gmail.com>
2022-06-03 00:58:53 +0600

    fix3

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 //importing routes
-const userRouter = require("./routes/user");
-const productRouter = require("./routes/products");
-const cartRouter = require("./routes/cart");
-const authRouter = require("./routes/auth");
-const orderRouter = require("./routes/order");
+const userRouter = require('./routes/user');
+const productRouter = require('./routes/products');
+const cartRouter = require('./routes/cart');
+const authRouter = require('./routes/auth');
+const orderRouter = require('./routes/order');
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
   })
 );
 
@@ -26,16 +26,16 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(console.log("succesfull"));
+  .then(console.log('succesfull'));
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("success creating server");
+  console.log('success creating server');
 });
 
-app.get('/' , (req,res)=> res.send('sdjhfgbjhdsb'))
+app.get('/', (req, res) => res.send('sdjhfgbjhdsb'));
 
-app.use("/api/user", userRouter);
-app.use("/api/products", productRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
+app.use('/api/user', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
